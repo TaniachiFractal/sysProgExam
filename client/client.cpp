5cb76cf9efc0899cd9025c6461ddc7dffd1a3f5b
TaniachiFractal <tnmslv@yandex.ru>
2023-12-22 21:06:04 +0300

    Add project files.

---
+++ client/client.cpp
@@ -0,0 +1,122 @@
+﻿#include <windows.h> 
+#include <iostream> 
+using namespace std;
+// главная функция с аргументами wchar_t
+int wmain(int argc, wchar_t* argv[])
+{
+	
+	/*
+	
+	ПОЛУЧЕНИЕ РАЗМЕРА МАССИВА
+	
+	*/
+	// Получаем handlы каналов
+	HANDLE ReadPipe = (HANDLE)_wtoi64(argv[1]);
+	HANDLE WritePipe = (HANDLE)_wtoi64(argv[2]);
+
+	DWORD BytesWritten, BytesRead;
+
+	Sleep(500);
+
+	long n = 0;
+	WriteFile(WritePipe, &n, sizeof(n), &BytesWritten, nullptr);
+
+	int arrLength = 0;
+
+	// Ожидаем записи в канал
+	WaitForSingleObject(ReadPipe, INFINITE);
+
+	// Читаем значение
+	ReadFile(ReadPipe, &arrLength, sizeof(arrLength), &BytesRead, nullptr);
+
+	// Выводим его в консоль
+	cout << "The array length is " << arrLength << endl;
+
+	HANDLE hStdOut; // дескриптор стандартного вывода 
+
+	cout << endl << endl;
+
+	/*
+
+	УСТАНОВКА КУРСОРА
+
+	*/
+	{
+		CONSOLE_CURSOR_INFO cci; // информация о курсоре 
+		// читаем дескриптор стандартного вывода 
+		hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		// читаем новый размер курсора 
+		cout << "Input a new size of cursor (1-100): ";
+		cin >> cci.dwSize;
+		// устанавливаем новый размер курсора 
+		if (!SetConsoleCursorInfo(hStdOut, &cci))
+			cout << "Set console cursor info failed." << endl;
+	}
+
+	cout << endl << endl;
+	/*
+
+	РАЗМЕР БУФЕРА ЭКРАНА
+
+	*/
+	{
+		COORD coord; // для размера буфера экрана 
+		// читаем дескриптор стандартного вывода 
+		hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		// вводим новый размер буфера экрана 
+		cout << "Enter new screen buffer size." << endl;
+		cout << "A number of columns (120+): ";
+		cin >> coord.X;
+		cout << "A number of rows (120+): ";
+		cin >> coord.Y;
+		// устанавливаем новый размер буфера экрана 
+		if (!SetConsoleScreenBufferSize(hStdOut, coord))
+		{
+			cout << "Set console screen buffer size failed." << endl;
+			return GetLastError();
+		}
+	}
+
+	float* arr = new float[arrLength];
+
+	cout << endl << endl;
+	/*
+
+	ГЕНЕРАЦИЯ ЧИСЕЛ
+
+	*/
+	{
+		int LO = 0;
+		int HI = 100;
+
+		cout << endl << endl;
+		cout << "Generating array: ";
+		srand(static_cast <unsigned> (time(0)));
+		for (int i = 0; i < arrLength; i++)
+		{
+			arr[i] = LO + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (HI - LO)));
+			cout << arr[i] << "    ";
+		}
+	}
+	cout << endl << endl;
+	/*
+
+	ОТПРАВКА ЧИСЕЛ
+
+	*/
+	{
+		cout << endl << endl;
+		cout << endl << "Send started" << endl;
+		for (int i = 0; i < arrLength; i++)
+		{
+			WriteFile(WritePipe, &arr[i], sizeof(arr[i]), &BytesWritten, nullptr);
+			cout << "Sent " << arr[i] << "; ";
+		}
+		cout << endl << "Send done." << endl;
+	}
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,217 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	/*
+	
+	ЗАПРОС РАЗМЕРА МАССИВА
+	
+	*/
+	int arrLength = 0;
+	{
+		cout << "Input the length of the array: ";
+		cin >> arrLength;
+	}
+
+	/*
+	
+	СОЗДАНИЕ МАССИВА
+	
+	*/
+	float* arr = new float[arrLength];
+	float* arrCorrect = new float[arrLength];
+
+	cout << endl << endl;
+	/*
+	
+	ОТПРАВКА ДЛИНЫ МАССИВА И ЕГО ПОЛУЧЕНИЕ
+	
+	*/
+	// Наndle каналов чтения и записи
+	HANDLE ReadPipe, WritePipe;
+
+	// Атрибуты безопасности, чтобы handlы были наследуемыми
+	SECURITY_ATTRIBUTES SecurityAttributes = { 0 };
+	SecurityAttributes.nLength = sizeof(SecurityAttributes);
+	SecurityAttributes.bInheritHandle = TRUE;
+	{
+		
+
+		// Создаём каналы чтения и записи, если успешно, продолжаем работу
+		if (CreatePipe(&ReadPipe, &WritePipe, &SecurityAttributes, 0))
+		{
+			// Информация о запускаемом процессе
+			STARTUPINFO StartupInfo = { 0 };
+			StartupInfo.cb = sizeof(StartupInfo);
+			PROCESS_INFORMATION ProcessInfo = { 0 };
+
+
+			// Путь к исполняемому файлу сервера и командная строка для запуска клиента
+			wchar_t Path[MAX_PATH], CmdLine[MAX_PATH];
+			// Получаем путь к исполняемому файлу сервера
+			GetModuleFileName(nullptr, Path, MAX_PATH);
+			// В последний слеш записываем нуль, тем самым усекаем строку
+			*wcsrchr(Path, '\\') = 0;
+			// получаем командную строку для запуска клиента
+			swprintf_s(CmdLine, L"\"%s\\%s\" %jd %jd", Path, L"client.exe", (long long)ReadPipe, (long long)WritePipe);
+
+			// Запускаем процесс клиента, в случае успеха продолжаем работу
+			if (CreateProcess(nullptr, CmdLine, nullptr, nullptr, true, CREATE_NEW_CONSOLE,
+				nullptr, nullptr, &StartupInfo, &ProcessInfo))
+			{
+				// Количество прочитанных и записанных байт
+				DWORD BytesRead, BytesWritten;
+
+				WaitForSingleObject(ReadPipe, INFINITE);
+				long n;
+				ReadFile(ReadPipe, &n, sizeof(n), &BytesRead, nullptr);
+
+
+
+				// Записываем данные в поток записи
+				WriteFile(WritePipe, &arrLength, sizeof(arrLength), &BytesWritten, nullptr);
+				
+				cout << endl << endl;
+
+				// Ожидаем завершения работы клиента
+				WaitForSingleObject(ReadPipe, INFINITE);
+				
+				// Читаем 
+				{
+					cout << "Receive started" << endl;
+					for (int i = 0; i < arrLength; i++)
+					{
+						ReadFile(ReadPipe, &arr[i], sizeof(arr[i]), &BytesRead, nullptr);
+						cout << "Received " << arr[i] << "; ";
+					}
+					cout << endl << "Receive done." << endl;
+				}
+				
+				// Ожидаем завершения клиентского процесса
+				WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
+				
+				// Закрываем handlы
+				CloseHandle(ProcessInfo.hProcess);
+				CloseHandle(ProcessInfo.hThread);
+			}
+			else // выдаём сообщение об ошибке
+				cout << "Process creation failed";
+
+			// закрываем handlы
+			CloseHandle(ReadPipe);
+			CloseHandle(WritePipe);
+
+		}
+		else // или выводим сообщение об ошибке
+			cout << "Pipe creation failed";
+
+	}
+
+	cout << endl << "Client's work is done. Booting Part now" << endl;;
+
+	/*
+	
+	РАБОТА С ПАРТОМ
+	
+	*/
+	int corrSize = 0;
+	{
+
+		// Создаём каналы чтения и записи, если успешно, продолжаем работу
+		if (CreatePipe(&ReadPipe, &WritePipe, &SecurityAttributes, 0))
+		{
+			// Информация о запускаемом процессе
+			STARTUPINFO StartupInfo = { 0 };
+			StartupInfo.cb = sizeof(StartupInfo);
+			PROCESS_INFORMATION ProcessInfo = { 0 };
+
+
+			// Путь к исполняемому файлу сервера и командная строка для запуска клиента
+			wchar_t Path[MAX_PATH], CmdLine[MAX_PATH];
+			// Получаем путь к исполняемому файлу сервера
+			GetModuleFileName(nullptr, Path, MAX_PATH);
+			// В последний слеш записываем нуль, тем самым усекаем строку
+			*wcsrchr(Path, '\\') = 0;
+			// получаем командную строку для запуска клиента
+			swprintf_s(CmdLine, L"\"%s\\%s\" %jd %jd", Path, L"part.exe", (long long)ReadPipe, (long long)WritePipe);
+
+			// Запускаем процесс клиента, в случае успеха продолжаем работу
+			if (CreateProcess(nullptr, CmdLine, nullptr, nullptr, true, CREATE_NEW_CONSOLE,
+				nullptr, nullptr, &StartupInfo, &ProcessInfo))
+			{
+				// Количество прочитанных и записанных байт
+				DWORD BytesRead, BytesWritten;
+
+				WaitForSingleObject(ReadPipe, INFINITE);
+				long n;
+				ReadFile(ReadPipe, &n, sizeof(n), &BytesRead, nullptr);
+
+				// Записываем данные в поток записи
+				WriteFile(WritePipe, &arrLength, sizeof(arrLength), &BytesWritten, nullptr);
+
+				// Пишем массив парту
+				cout << endl << "Send started" << endl;
+				for (int i = 0; i < arrLength; i++)
+				{
+					WriteFile(WritePipe, &arr[i], sizeof(arr[i]), &BytesWritten, nullptr);
+					cout << "Sent " << arr[i] << "; ";
+				}
+				cout << endl << "Send done." << endl;
+
+				cout << endl << endl;
+
+				// Ожидаем завершения работы клиента
+				WaitForSingleObject(ReadPipe, INFINITE);
+				// Читаем длину
+				
+				ReadFile(ReadPipe, &corrSize, sizeof(corrSize), &BytesRead, nullptr);
+				// Читаем 
+				{
+					cout << "Receive started" << endl;
+					for (int i = 0; i < corrSize; i++)
+					{
+						ReadFile(ReadPipe, &arrCorrect[i], sizeof(arrCorrect[i]), &BytesRead, nullptr);
+						cout << "Received " << arrCorrect[i] << "; ";
+					}
+					cout << endl << "Receive done." << endl;
+				}
+
+
+				// Ожидаем завершения клиентского процесса
+				WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
+
+				// Закрываем handlы
+				CloseHandle(ProcessInfo.hProcess);
+				CloseHandle(ProcessInfo.hThread);
+			}
+			else // выдаём сообщение об ошибке
+				cout << "Process creation failed";
+
+			// закрываем handlы
+			CloseHandle(ReadPipe);
+			CloseHandle(WritePipe);
+
+		}
+		else // или выводим сообщение об ошибке
+			cout << "Pipe creation failed";
+
+	}
+
+	cout << endl << endl;
+	cout << "Final array: ";
+	for (int i = 0; i < corrSize; i++)
+		{
+		cout << arrCorrect[i]<<" ";
+		}
+
+
+	cout << endl << endl;
+	
+
+	cout << "The work is done. Press Enter to exit" << endl;
+	system("pause");
+	return 0;
+}
+
